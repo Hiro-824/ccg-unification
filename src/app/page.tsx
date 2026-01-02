df0305e9a0dab6316194ec70e50db4ba25ef8aa4
Hiro-824 <thefilepreserver@gmail.com>
2026-01-02 21:34:16 +0900

    Remove complex lexicon and parsing logic from Home component, replacing it with a simple "Hello World" message for a cleaner UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
-import { CombinatoryCategorialGrammar } from "../grammar/ccg";
-import { atom, complex, Lexicon } from "../lexicon/lexicon";
-import { parse } from "../parser/parser";
-
 export default function Home() {
-
-  const lexicon = new Lexicon();
-  lexicon.add("John", [atom("NP"), complex(atom("S"), "/", complex(atom("S"), "\\", atom("NP")))]);
-  lexicon.add("Mary", [atom("NP"), complex(atom("S"), "/", complex(atom("S"), "\\", atom("NP")))]);
-  lexicon.add("apples", [atom("NP")]);
-  lexicon.add("might", [complex(complex(atom("S"), "\\", atom("NP")), "/", complex(atom("S"), "\\", atom("NP")))]);
-  lexicon.add("likes", [complex(complex(atom("S"), "\\", atom("NP")), "/", atom("NP"))]);
-  lexicon.add("loves", [complex(complex(atom("S"), "\\", atom("NP")), "/", atom("NP"))]);
-  const X = complex(atom("S"), "/", atom("NP"));
-  lexicon.add("and", [
-    complex(
-      complex(X, "\\", X),
-      "/",
-      X
-    )
-  ]);
-  const grammar = new CombinatoryCategorialGrammar(lexicon);
-  const result = parse(["John", "likes", "and", "Mary", "loves", "apples"], grammar);
-
   return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Parse Result</h1>
-      <pre>{JSON.stringify(result, null, 2)}</pre>
-    </main>
+    <>Hello World</>
   );
 }
