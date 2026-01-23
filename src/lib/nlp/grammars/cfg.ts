2d87fb45d5c33ed19d51debb8d7f088be6b57e9c
Hiro-824 <thefilepreserver@gmail.com>
2026-01-23 15:21:27 +0900

    Implement CFG class.

---
+++ src/lib/nlp/grammars/cfg.ts
@@ -0,0 +1,27 @@
+import { Grammar } from "../core/parser";
+
+export class CFG implements Grammar<string> {
+
+    lexicon: Map<string, string> = new Map();
+
+    constructor() {
+        this.lexicon.set("john", "NP");
+        this.lexicon.set("sees", "V");
+        this.lexicon.set("mary", "NP");
+    }
+
+    getAvailableWords(): string[] {
+        return [...this.lexicon.keys()];
+    }
+
+    getTerminalCategories(word: string): string[] {
+        const category = this.lexicon.get(word);
+        return category ? [category] : [];
+    }
+    
+    combine(left: string, right: string): string[] {
+        if(left === "NP" && right === "VP") return ["S"];
+        if(left === "V" && right === "NP") return ["VP"];
+        return [];
+    }
+}
