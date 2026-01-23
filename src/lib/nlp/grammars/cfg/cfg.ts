f44661b588319b62bc0b9b61ae5e87cce9af7bd4
Hiro-824 <thefilepreserver@gmail.com>
2026-01-23 17:18:03 +0900

    Add rules involving prepositions and determiners to see ambiguity.

--- src/lib/nlp/grammars/cfg/cfg.ts
+++ src/lib/nlp/grammars/cfg/cfg.ts
@@ -6,8 +6,12 @@ export class CFG implements Grammar<string> {
 
     constructor() {
         this.lexicon.set("john", "NP");
-        this.lexicon.set("sees", "V");
         this.lexicon.set("mary", "NP");
+        this.lexicon.set("sees", "V");
+        this.lexicon.set("girl", "N");
+        this.lexicon.set("telescope", "N");
+        this.lexicon.set("a", "Det");
+        this.lexicon.set("with", "P");
     }
 
     getAvailableWords(): string[] {
@@ -22,6 +26,10 @@ export class CFG implements Grammar<string> {
     combine(left: string, right: string): string[] {
         if(left === "NP" && right === "VP") return ["S"];
         if(left === "V" && right === "NP") return ["VP"];
+        if(left === "Det" && right === "N") return ["NP"];
+        if(left === "P" && right === "NP") return ["PP"];
+        if(left === "VP" && right === "PP") return ["VP"];
+        if(left === "N" && right === "PP") return ["N"];
         return [];
     }
 }
